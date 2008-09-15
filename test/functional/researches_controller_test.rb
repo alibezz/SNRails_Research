2d0cde34d0624eca0c9f752e6175e99f17c51a94
Leandro Nunes dos Santos <leandronunes@safernet.org.br>
2008-09-15 17:19:27 -0300

    making functional tests

--- test/functional/researches_controller_test.rb
+++ test/functional/researches_controller_test.rb
@@ -11,6 +11,8 @@ class ResearchesControllerTest < Test::Unit::TestCase
     @controller = ResearchesController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    env = Environment.new
+    Environment.expects(:default).returns(env)
   end
 
   def test_should_get_index
@@ -19,39 +21,9 @@ class ResearchesControllerTest < Test::Unit::TestCase
     assert assigns(:researches)
   end
 
-  def test_should_get_new
-    get :new
-    assert_response :success
-  end
-  
-  def test_should_create_research
-    old_count = Research.count
-    post :create, :research => { }
-    assert_equal old_count+1, Research.count
-    
-    assert_redirected_to research_path(assigns(:research))
-  end
-
   def test_should_show_research
     get :show, :id => 1
     assert_response :success
   end
 
-  def test_should_get_edit
-    get :edit, :id => 1
-    assert_response :success
-  end
-  
-  def test_should_update_research
-    put :update, :id => 1, :research => { }
-    assert_redirected_to research_path(assigns(:research))
-  end
-  
-  def test_should_destroy_research
-    old_count = Research.count
-    delete :destroy, :id => 1
-    assert_equal old_count-1, Research.count
-    
-    assert_redirected_to researches_path
-  end
 end
