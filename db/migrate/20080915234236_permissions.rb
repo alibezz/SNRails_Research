98436d39bf62f02c457c186505fe4281d1588956
Leandro Nunes dos Santos <leandronunes@safernet.org.br>
2008-09-15 21:39:36 -0300

    adding permission control support

---
+++ db/migrate/20080915234236_permissions.rb
@@ -0,0 +1,13 @@
+class Permissions < ActiveRecord::Migration
+  def self.up
+    create_table :permissions, :force => true do |t|
+      t.references :research
+      t.references :user
+      t.boolean :is_moderator
+    end
+  end
+
+  def self.down
+    drop_table :permissions
+  end
+end
