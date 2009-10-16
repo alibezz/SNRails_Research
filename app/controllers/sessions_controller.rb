9b15d67e95c3602cef50ef084bad45a63db1fe3e
Caio Tiago Oliveira <caiotiago@safernet.org.br>
2009-10-16 10:42:39 -0300

    Using 't' in place of '_'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
       redirect_back_or_default('/')
-      flash[:notice] = _("Logged in successfully")
+      flash[:notice] = t("Logged in successfully")
     else
       note_failed_signin
       @login       = params[:login]
@@ -30,14 +30,14 @@ class SessionsController < ApplicationController
 
   def destroy
     logout_killing_session!
-    flash[:notice] = _("You have been logged out.")
+    flash[:notice] = t("You have been logged out.")
     redirect_back_or_default('/')
   end
 
 protected
   # Track failed login attempts
   def note_failed_signin
-    flash[:error] = _("Couldn't log you in as '#{params[:login]}'")
+    flash[:error] = t("Couldn't log you in as '#{params[:login]}'")
     logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
 end
