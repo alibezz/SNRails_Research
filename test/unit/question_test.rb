e991146feb8ec1371b84527dd84484c8f21ac1a6
Aline Bessa <alinebessa@Mysrtara04.(none)>
2009-10-28 10:13:56 -0300

    question.rb and research.rb tested.

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -1,7 +1,17 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class QuestionTest < Test::Unit::TestCase
-  def test_truth
-    assert true
+  
+  def test_should_have_info
+    research = create_research
+
+    count = research.items.length
+    question = Question.create(:research_id => research.id, :info => nil, :position => count + 1)
+    research.reload
+    assert_equal count, research.items.length	
+
+    question = create_item(:type => 'question', :research_id => research.id, :info => 'info')
+    research.reload
+    assert_equal count + 1, research.items.length	
   end
 end
