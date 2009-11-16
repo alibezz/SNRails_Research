0ff5bef76c7298d96845ebd88d1161561f4a2687
Aline Bessa <alinebessa@Mystara05.(none)>
2009-11-16 10:27:54 -0300

    Item_values can be updated according to other item_values position. They appear ordered by their position in the item index. No view refer to their positions explicitely.

--- app/controllers/admin/item_values_controller.rb
+++ app/controllers/admin/item_values_controller.rb
@@ -4,18 +4,23 @@ class Admin::ItemValuesController < ResourceController::Base
 
   before_filter :load_research, :only => :index
   before_filter :load_item
-  
   before_filter :load_item_values_position
 
   index.before do
-    @item_values = @item.item_values
+    #FIXME item_values should be kept sorted since their creation!!
+    @item_values = @item.item_values.sort { |a,b| a.position <=> b.position }
   end
 
   create.before do
-    require 'pp'
-    pp params
     @item.reorder_item_values(params[:item_value][:position].to_i)
   end
+
+  update.before do
+    require 'pp'
+    pp  ItemValue.find(params[:id]).position
+    @item.update_positions(params[:item_value][:position].to_i, ItemValue.find(params[:id]).position)
+  end
+
 
   def collection
     @collection ||= @research.items.find(params[:item_id]).item_values
