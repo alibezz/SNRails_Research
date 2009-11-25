924ff25863932ca02e0b0d7dcc8ee12262674542
Aline Bessa <alinebessa@Mystara05.(none)>
2009-11-25 15:18:20 -0300

    questionnaire receives answers on create - no more on update

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -2,8 +2,9 @@ class QuestionnairesController < ResourceController::Base
   belongs_to :research
   before_filter :load_research
 
-  update.before do
+  create.after do
    @questionnaire.associate(params[:object_item_values])
   end
 
+  create.wants.html { redirect_to research_path(@research) }
 end
