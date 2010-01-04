043160eb9735ec2d0f300ceca8de9c6e34ef07be
Leandro Nunes dos Santos <leandronunes@gmail.com>
2010-01-04 10:54:24 -0300

    adding AuthenticatedTestHelper

---
+++ lib/authenticated_test_helper.rb
@@ -0,0 +1,15 @@
+module AuthenticatedTestHelper
+  # Sets the current user in the session from the user fixtures.
+  def login_as(user)
+    @request.session[:user_id] = user ? User.find_by_login(user).id : nil
+  end
+
+	def logout
+		@request.session[:user_id] = nil
+	end
+
+  def authorize_as(user)
+    @request.env["HTTP_AUTHORIZATION"] = user ? ActionController::HttpAuthentication::Basic.encode_credentials(users(user).login, 'monkey') : nil
+  end
+  
+end
