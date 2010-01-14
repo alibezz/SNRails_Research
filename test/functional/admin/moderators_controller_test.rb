5f3873f0f11bccb8353d07a1ecbd45ff591aacbc
Aline Bessa <alinebessa@Mysrtara04.(none)>
2010-01-14 10:04:36 -0300

    Moderators controller tests passing.

--- test/functional/admin/moderators_controller_test.rb
+++ test/functional/admin/moderators_controller_test.rb
@@ -12,94 +12,68 @@ class ModeratorsControllerTest < Test::Unit::TestCase
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     @environment = Environment.create(:is_default => true)
-#    Environment.expects(:default).returns(env)
+    Environment.expects(:default).returns(@environment)
     login_as :quentin
+    @research = create_research
   end
 
 #index
 
   def test_should_get_index
-    research = create_research
-    get :index, :research_id => research.id
+    get :index, :research_id => @research.id
     assert_response :success
     assert_template 'index'
     assert assigns(:users)
   end
 
-  def test_index_should_show_link_to_add_moderators
-    research = create_research
-    get :index, :research_id => research.id
+  def test_index_should_show_link_to_update_moderators
+    get :index, :research_id => @research.id
     assert_tag :tag => "ul", :descendant => { :tag => "li" }
-    assert_tag :tag => 'a', :attributes => { :href => new_admin_research_moderator_url(research.id) }
-  end
-
-  def test_should_not_get_all_users_on_index
-    user1 = create_user(:login => 'user1')
-    r = create_research
-
-    # The user added is not a moderator
-    r.users<< user1
-   
-    get :index, :research_id => r.id
-    assert_response :success
-    assert assigns(:users)
-    assert_equal 1, r.users.count
-    assert_equal 0, assigns(:users).length, "The user added is not a moderator"
+    assert_tag :tag => 'a', :attributes => { :href => new_admin_research_moderator_url(@research.id) }
   end
     
   def test_should_get_only_moderators_on_index
-    r = create_research
 
-    user1 = create_user(:login => 'user1')
     # This user is not a moderator
-    r.users<< user1
+    user1 = create_user(:login => 'user1')
 
     # adding moderators users
     user2 = create_user(:login => 'user2')
     user3 = create_user(:login => 'user3')
-    r.moderator_permissions.create(:user => user2)
-    r.moderator_permissions.create(:user => user3)
+    @research.moderator_permissions.create(:user => user2)
+    @research.moderator_permissions.create(:user => user3)
 
-    get :index, :research_id => r.id
+    get :index, :research_id => @research.id
     assert_response :success
     assert assigns(:users)
-    assert_equal 3, assigns(:research).users.count
     assert_equal 2, assigns(:users).length, "There are 2 moderatores on this research"
   end
 
 #new
 
   def test_should_get_new
-    r = create_research
-
-    get :new, :research_id => r.id
+    get :new, :research_id => @research.id
     assert_response :success
     assert_template 'new'
   end
 
   def test_new_should_have_form
-    r = create_research
-    get :new, :research_id => r.id
+    get :new, :research_id => @research.id
     assert_tag :tag => 'form', :attributes => { :method => 'post' }    
   end
 
-  def test_new_should_have_back_link
-    r = create_research
-    get :new, :research_id => r.id
-    assert_tag :tag => 'a', :attributes => { :href => admin_research_moderators_url(r.id) }    
-  end
+#update
 
-#create
+  def test_should_update_moderators_properly
+    user1 = create_user(:login => 'user1')
+    user2 = create_user(:login => 'user2')
+    user3 = create_user(:login => 'user3')
+    
+    assert_equal 0, @research.moderator_permissions.count
+    post :update, :moderator_ids => [user1.id.to_s, user2.id.to_s], :research_id => @research.id
+    
+    @research.reload 
 
-  def test_should_post_create_successfully
-    r = create_research
-
-    assert_equal 0, r.moderator_permissions.length
-    post :create, :user => user_params, :research_id => r.id
-    assert_response :redirect
-    assert_redirected_to admin_research_moderators_url(r)
-    r.reload 
-
-    assert_equal 1, r.moderator_permissions.length
+    assert_equal 2, @research.moderator_permissions.length
   end
 end
