47420bf0b520f4adcaea70a644ed6f6015b738f5
Caio Tiago Oliveira <caiotiago@safernet.org.br>
2010-07-28 16:51:02 -0300

    Adding span around 'move' 'button'

--- app/helpers/survey/surveys_helper.rb
+++ app/helpers/survey/surveys_helper.rb
@@ -10,7 +10,7 @@ module Survey::SurveysHelper
   def paginate_survey(survey, page)
     pages = []
     survey.page_ids.each do |p|
-      pages << content_tag(:li, "#{t(:move)} #{link_to(p, {:page => p})}",  {:id => "item_#{p}"})
+      pages << content_tag(:li, "<span>#{t(:move)}</span> #{link_to(p, {:page => p})}",  {:id => "item_#{p}"})
     end
     content_tag(:ul, pages, {:id => 'page_links'})
   end
